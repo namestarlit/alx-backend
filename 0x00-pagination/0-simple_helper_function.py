1e2a49283e0e7e6f9ef6e135b41c695e2af377ba
Paul John <rainson.work@gmail.com>
2024-01-01 12:24:23 +0300

    Add 0-simple_helper_function.py

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A seek pagination helper function to return start and end list indexes"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns a start and end indexes of a list"""
+    # Compute the start index of the list
+    start_index = (page -1) * page_size
+
+    # Compute the end index of the list
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
