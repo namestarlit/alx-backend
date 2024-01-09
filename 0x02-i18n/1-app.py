c847a0045663ff60be7d2c5794f9e976d60b1ed4
Paul John <rainson.work@gmail.com>
2024-01-09 19:10:00 +0300

    1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""A Flask application"""
+from flask import Flask
+from flask_babel import Babel
+from flask import render_template
+
+from config import Config
+
+# Instantiate the flask object
+app = Flask(__name__)
+# Set configuration from Config class
+app.config.from_object(Config)
+
+# Instantiate babel object
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """returns the index page"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
