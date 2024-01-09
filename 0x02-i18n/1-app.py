7410fa5b9bff80c777c791c33f1a05d2d4b5ce8d
Paul John <rainson.work@gmail.com>
2024-01-09 19:12:12 +0300

    Modify the rendered page

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -18,7 +18,7 @@ babel = Babel(app)
 @app.route("/")
 def index():
     """returns the index page"""
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
